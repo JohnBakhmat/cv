4eac910b202986056ca0581d71e2cbabdcdf67f8
John bakhmat <johnbakhmat@gmail.com>
2024-01-30 21:48:48 +0300

    customization

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,170 @@
-import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
-} from "@/images/logos";
-import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
+import { NSNLogo, ParabolLogo } from "@/images/logos";
+import { GitHubIcon, XIcon } from "@/components/icons";
+import { TelegramIcon } from "@/components/icons/TelegramIcon";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Евгений Бахмат",
+  initials: "Е.Ю.",
+  location: "Москва, Россия, GMT+2",
+  locationLink: "https://www.google.com/maps/place/Moscow",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Full Stack Engineer специализирующийся на создании продуктов с особым вниманием к деталям.",
+  summary: `
+  Всегда интересуюсь последними тенденциями в веб-разработке. Предпочитаю понятный код и простые решения сложных проблем.
+  Могу эффективно управлять собой в рамках независимых проектов, а также сотрудничать в составе команды.
+  Помимо разработки, занимаюсь поддержанием и переводом документации Open-Source проектов.
+  `,
+  avatarUrl: "https://avatars.githubusercontent.com/u/34962307?v=4",
+  personalWebsiteUrl: "https://johnbakhmat.tech",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "johnbakhmat@gmail.com",
+    tel: "+79915888792",
     social: [
       {
+        name: "Telegram",
+        url: "https://t.me/evheniy_bakhmat",
+        icon: TelegramIcon,
+      },
+      {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/johnbakhmat",
         icon: GitHubIcon,
       },
       {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
-        icon: LinkedInIcon,
-      },
-      {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/johnbakhmat",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "Харьковский национальный университет радиоэлектроники",
+      degree: "Степень бакалавра в области разработки программного обеспечения",
+      start: "2019",
+      end: "2023",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "CareerHub",
+      link: "http://rabota.nure.ua",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      title: "Frontend Developer",
       logo: ParabolLogo,
       start: "2021",
-      end: "2024",
+      end: "2023",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "Планирование, разработка и тестирование новых функция для системы поиска работы. Работал над миграцией проекта из формата React SPA в Next SSR/SSG. Организовал DevOps workflow для упрощения разработки и развертования кода. Технологии: React, TypeScript, Next.js.",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
+      company: "Sigma Software",
+      link: "https://sigma.software",
       badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
+      title: "Junior Frontend Developer",
       logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      start: "2022",
+      end: "2022",
+      description: "Разработка и тестирование для innersource.",
     },
   ],
   skills: [
     "JavaScript",
     "TypeScript",
-    "React/Next.js/Remix",
+    "React/Next.js",
+    "Vue/Nuxt",
+    "Astro",
     "Node.js",
     "GraphQL",
     "Relay",
-    "WebRTC",
   ],
   projects: [
     {
-      title: "Consultly",
-      techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
-      ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      title: "johnbakhmat.tech",
+      description: "Мой персональный сайт.",
+      techStack: ["TypeScript", "Astro", "Tailwind", "React", "UnoCSS"],
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "johnbakhmat.tech",
+        href: "https://www.johnbakhmat.tech/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "CareerHub",
+      techStack: ["TypeScript", "Next.js", "SCSS", "Redux"],
+      description: "Платформа для помощи студентам ХНУРЭ в поиске работы.",
+    },
+    {
+      title: "ChemSolution",
+      techStack: ["TypeScript", "Vue", "Tailwind", "Vuex", "Vue Router"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Платформа изучения химии, использующая принципы геймификации обучения.",
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "ChemSolution Repo",
+        href: "https://github.com/johnbakhmat/chemsolution",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+      title: "Create T3 App",
+      techStack: ["Astro", "TypeScript", "Markdown"],
+      description: "Перевод документацию проекта на русский язык.",
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "Create T3 App",
+        href: "https://create.t3.gg",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+      title: "Happier Ever After",
+      techStack: ["C#", "Unity", "Ldtk", "Aseprite"],
+      description: "Игра в жанре Stealth Roguelike Platformer.",
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "HappierEverAfter Repo",
+        href: "https://github.com/johnbakhmat/HappierEverAfter",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
+      title: "Pinned",
+      techStack: ["Go", "Fiber", "GraphQL"],
+      description: "Сервис для получения закрепленных репзиториев Github.",
       link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
+        label: "Pinned Repo",
+        href: "https://github.com/johnbakhmat/pinned",
       },
     },
     {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
+      title: "Asciifier",
+      techStack: ["Go"],
+      description: "Утилита создания ASCII изображений.",
       link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
+        label: "Asciifier Repo",
+        href: "https://github.com/johnbakhmat/asciifier",
       },
     },
     {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+      title: "Rust Pong",
+      techStack: ["Rust", "Bevy"],
+      description: "Имплементация игры Pong с помощью Rust и Bevy.",
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "rust-pong Repo",
+        href: "https://github.com/johnbakhmat/rust-pong",
       },
     },
     {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+      title: "Ultimate Rock Paper Scissors",
+      techStack: ["TypeScript", "Next.js", "tRPC", "Socket.io", "Tailwind"],
+      description: 'Улучшеная версия "Камень, Ножницы, Бумага" с мультиплеером',
       link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
+        label: "ultimate-rps Repo",
+        href: "https://github.com/johnbakhmat/ultimate-rps-turbo",
       },
     },
     {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
+      title: "FloristiX Flowers",
+      techStack: ["JavaScript", "Vue", "Vuex", "Vue Router", "Tailwind"],
+      description: "Сервис продажи цветов.",
       link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
+        label: "floristix flowers Repo",
+        href: "https://github.com/johnbakhmat/floristix-flowers",
       },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
